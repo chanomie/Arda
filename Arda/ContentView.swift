b8b5bd10967884b2cf71236e936a4ee10c1cec4c
Jordan <2756092+chanomie@users.noreply.github.com>
2020-02-05 21:05:24 -0800

    Initial Commit

---
+++ Arda/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Arda
+//
+//  Created by Jordan Reed on 2/5/20.
+//  Copyright © 2020 Jordan Reed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
